7a3d0c2121c0c4f632b0a5c2d4d01839be48f6e6
Shenzhi Chen <167293516+Shenzhi-Chen@users.noreply.github.com>
2025-11-11 11:21:44 +0100

    Update Run_models.sh

--- Enhancer_activity_model_training/Run_models.sh
+++ Enhancer_activity_model_training/Run_models.sh
@@ -3,51 +3,47 @@ tissue_list="midbrain,heart,limb"
 fold_list="fold01,fold02,fold03"
 script_path=Enhancer_activity_model_training
 InputID=enhancer_activity_model_dataset
-TFModisco=1
 for fold in ${fold_list//,/ }; do
   for tissue in ${tissue_list//,/ }; do
     for rep in 1 2; do
-
-          arch=result/model/accessibility_model/${tissue}/results_${fold}_${tissue}_DeepSTARR_rep${rep}
-					OUTDIR=result/model/enhancer_activity_model/${tissue}/results_${fold}_${tissue}_DeepSTARR_rep${rep}
-
-          mkdir ${OUTDIR}
-					mkdir -p ${OUTDIR}/log_training
-					JOB_ID=VISTA_${tissue}
-					scripts/functions/my_bsub_gridengine -P g \
-                                     -G "gpu:1" \
-                                     -m 60 \
-                                     -T '3:00:00' \
-                                     -o ${OUTDIR}/log_training \
-                                     -n transfer_learning_${JOB_ID} "${script_path}/Train_transfer_learning_model.py \
-                                     -i ${InputID}/${tissue}/${fold} \
-                                     -v class \
-                                     -a ${arch}/Model \
-                                     -o ${OUTDIR}/Model" > ${OUTDIR}/log_training/msg.model_training.tmp
-
-					# Predict enhancer test dataset
-					mkdir -p ${OUTDIR}/log_prediction
-					JOB_ID=act_VISTA_pred_${ID}_init_random
-					scripts/functions/my_bsub_gridengine -n ${JOB_ID} \
-                                     -o ${OUTDIR}/log_predictions \
-                                     -m 80 \
-                                     -T '4:00:00' \
-                                     "${script_path}/Predict_CNN_model_from_fasta.py \
-                                     -s ${InputID}/${tissue}/${fold}_sequences_test.fa \
-                                     -m ${OUTDIR}/Model \
-                                     -o ${OUTDIR}"  
+		arch=result/model/accessibility_model/${tissue}/results_${fold}_${tissue}_DeepSTARR_rep${rep}
+		OUTDIR=result/model/enhancer_activity_model/${tissue}/results_${fold}_${tissue}_DeepSTARR_rep${rep}
+		mkdir ${OUTDIR}
+		mkdir -p ${OUTDIR}/log_training
+		JOB_ID=VISTA_${tissue}
+		scripts/functions/my_bsub_gridengine -P g \
+											 -G "gpu:1" \
+											 -m 60 \
+											 -T '3:00:00' \
+											 -o ${OUTDIR}/log_training \
+											 -n transfer_learning_${JOB_ID} "${script_path}/Train_transfer_learning_model.py \
+											 -i ${InputID}/${tissue}/${fold} \
+											 -v class \
+											 -a ${arch}/Model \
+											 -o ${OUTDIR}/Model" > ${OUTDIR}/log_training/msg.model_training.tmp
+		# Predict enhancer test dataset
+		mkdir -p ${OUTDIR}/log_predictions
+		JOB_ID=act_VISTA_pred_${ID}_init_random
+		scripts/functions/my_bsub_gridengine -n ${JOB_ID} \
+											 -o ${OUTDIR}/log_predictions \
+											 -m 80 \
+											 -T '4:00:00' \
+											 "${script_path}/Predict_CNN_model_from_fasta.py \
+											 -s ${InputID}/${tissue}/${fold}_sequences_test.fa \
+											 -m ${OUTDIR}/Model \
+											 -o ${OUTDIR}"  
                                      
-					# Predict random sequences
-					scripts/functions/my_bsub_gridengine -n act_predict_random_${Input_ID}_init_random \
-                                     -o ${OUTDIR}/log_predictions \
-                                     -m 80 \
-                                     -T '4:00:00' \
-                                     "${script_path}/Predict_CNN_model_from_fasta.py \
-                                     -s testing_dataset/random_sequences_600k.fasta \
-                                     -m ${OUTDIR}/Model \
-                                     -o ${OUTDIR}"                                      
+		# Predict random sequences
+		scripts/functions/my_bsub_gridengine -n act_predict_random_${Input_ID}_init_random \
+											 -o ${OUTDIR}/log_predictions \
+											 -m 80 \
+											 -T '4:00:00' \
+											 "${script_path}/Predict_CNN_model_from_fasta.py \
+											 -s testing_dataset/random_sequences_600k.fasta \
+											 -m ${OUTDIR}/Model \
+											 -o ${OUTDIR}"                                      
 
-          # contribution scores
+         # contribution scores
          scripts/functions/my_bsub_gridengine -m 60 \
                                        -T '10:00:00' \
                                        -P g -G "gpu:3" \
